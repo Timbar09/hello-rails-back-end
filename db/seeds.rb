855a6955b8eca66d01e31099cd6dcfd7917bc37a
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-16 11:13:17 +0200

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  "Hello!",
+  "Hi!",
+  "Hey!",
+  "Howdy!",
+  "Yo!",
+  "What's up?",
+  "Greetings!",
+  "Salutations!",
+  "Good day!",
+  "Good morning!",
+  "Good afternoon!",
+  "Good evening!",
+  "Good night!",
+  "How are you?",
+  "How's it going?",
+  "How are things?",
+  "How's life?",
+  "How's everything?",
+  "How's your day?",
+  "How's your day going?",
+  "How have you been?",
+  "What's new?",
+  "What's going on?",
+  "What's happening?",
+]
+
+greetings.each do |greeting|
+  Greeting.create(greeting: greeting)
+end
